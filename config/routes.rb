895db6bceea5a25a22fbb78b92c0f05e98e35ff6
Domikado 4 <domikado4@Domikado-4.local>
2013-12-11 16:31:33 +0700

    Fix conflict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 WorldCupApp::Application.routes.draw do
   root 'nations#index'
   devise_for :users
-<<<<<<< HEAD
-  resources :nations, :players, :groups, :matches
+
+  resources :nations, :players, :coaches, :referees, :stadiums, :groups, :matches
   get '/matches/:id/edit_score', to: 'matches#edit_score'
   get '/matches/:id/update_score', to: 'matches#update_score'
-=======
-  resources :nations, :players, :coaches, :referees, :stadiums
->>>>>>> 8f55cba8b1618749a75db12ad557889d11a8e403
 end  
   
