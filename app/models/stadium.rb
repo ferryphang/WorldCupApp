8f55cba8b1618749a75db12ad557889d11a8e403
Domikado 3 <domikado3@Domikado-3.local>
2013-12-11 14:47:00 +0700

    validate of stadiums

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -2,4 +2,5 @@ class Stadium < ActiveRecord::Base
 	# Sebuah stadium memiliki banyak pertandingan
   # jadi sebuah pertandingan yang berbeda bisa terjadi pada satu stadium 
   has_many :matches
+  validates :name , presence:true, uniqueness: true
 end
